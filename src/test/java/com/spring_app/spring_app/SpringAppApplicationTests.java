c9bcf25f3756a1e51216a248faf8d09a33f3f292
verysmartsabari07 <sabarilal.18@gmail.com>
2024-03-07 04:18:16 +0530

    first commit

---
+++ src/test/java/com/spring_app/spring_app/SpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_app.spring_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
